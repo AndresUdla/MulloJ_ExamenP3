f55fdd3c354f77f4de27e13a18adece43ebea518
Josue Mullo <josue.mullo@udla.edu.ec>
2025-07-07 07:37:10 -0500

    DatabaseService añadido para la creacion de la base de datos local, ademas de instanciar una sola vez con la propiedad static al SuscripcionRepository

---
+++ MulloJ_ExamenP3/Services/DatabaseService.cs
@@ -0,0 +1,27 @@
+﻿using MulloJ_ExamenP3.Repositories;
+
+namespace MulloJ_ExamenP3.Services
+{
+    public class DatabaseService
+    {
+        private static SuscripcionRepository _suscripcionRepository;
+
+        public static SuscripcionRepository SuscripcionRepository
+        {
+            get
+            {
+                if (_suscripcionRepository == null)
+                {
+                    var dbPath = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "suscripciones_MulloJ.db3"
+                    );
+
+                    _suscripcionRepository = new SuscripcionRepository(dbPath);
+                }
+
+                return _suscripcionRepository;
+            }
+        }
+    }
+}
