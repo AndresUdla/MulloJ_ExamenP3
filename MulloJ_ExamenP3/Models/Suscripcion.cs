e80242c55f87c51f2425b3e2c994abd53a6373a1
Josue Mullo <josue.mullo@udla.edu.ec>
2025-07-07 07:22:27 -0500

    Modelo Suscripcion.cs creado con sus rescptivos atributos ademas de un Id

---
+++ MulloJ_ExamenP3/Models/Suscripcion.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+
+namespace MulloJ_ExamenP3.Models
+{
+    public class Suscripcion
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        [MaxLength(100)]
+        public string Servicio { get; set; }
+
+        [NotNull]
+        [MaxLength(100)]
+        public string CorreoAsociado { get; set; }
+
+        [NotNull]
+        public decimal CostoMensual { get; set; }
+
+        [NotNull]
+        public bool RenovacionAutomatica { get; set; }
+    }
+}
