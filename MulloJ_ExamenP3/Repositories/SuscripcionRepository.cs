31d84a62f4e22a1e6d7e4e1f4f352a5721683c18
Josue Mullo <josue.mullo@udla.edu.ec>
2025-07-07 07:29:23 -0500

    Creacion de SuscripcionRepository.cs para manejo de la base de datos que se creara posteriormente

---
+++ MulloJ_ExamenP3/Repositories/SuscripcionRepository.cs
@@ -0,0 +1,36 @@
+﻿using SQLite;
+using MulloJ_ExamenP3.Models;
+
+namespace MulloJ_ExamenP3.Repositories
+{
+    public class SuscripcionRepository
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public SuscripcionRepository(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Suscripcion>().Wait();
+        }
+
+        public Task<List<Suscripcion>> ObtenerSuscripcionesAsync()
+        {
+            return _database.Table<Suscripcion>().ToListAsync();
+        }
+
+        public Task<int> AgregarSuscripcionAsync(Suscripcion suscripcion)
+        {
+            return _database.InsertAsync(suscripcion);
+        }
+
+        public Task<int> EliminarSuscripcionAsync(Suscripcion suscripcion)
+        {
+            return _database.DeleteAsync(suscripcion);
+        }
+
+        public Task<int> ActualizarSuscripcionAsync(Suscripcion suscripcion)
+        {
+            return _database.UpdateAsync(suscripcion);
+        }
+    }
+}
